9296b24afc957726f017bc8f27e7a63626e523db
wang <wlcupup@126.com>
2018-04-26 16:47:50 +0800

    UP

--- app/models.py
+++ app/models.py
@@ -1,2 +1,7 @@
 from flask import current_app, request, url_for
+from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
